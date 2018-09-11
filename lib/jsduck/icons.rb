9e783db40605fdd5b95bda379e142932c4e03654
Christopher Williams <chris.a.williams@gmail.com>
2018-09-11 10:52:26 -0400

    Don't set private/isObject/extends values in JSON unless they're non-null - shrinks generated data JS file

--- lib/jsduck/icons.rb
+++ lib/jsduck/icons.rb
@@ -6,25 +6,26 @@ module JsDuck
     def create(classes, opts)
       json = []
       classes.map do |cls|
-        json.push({
+        obj = {
           :name => cls[:name],
-          :extends => cls[:extends],
-          :private => cls[:private] || cls[:meta][:pseudo],
-          :icon => Icons::class_icon(cls),
-          :isObject => isObject(cls)
-        })
+          :icon => Icons::class_icon(cls)
+        }
+        obj[:private] = true if cls[:private] || cls[:meta][:pseudo]
+        obj[:isObject] = true if isObject(cls)
+        obj[:extends] = cls[:extends] if cls[:extends]
+        json.push(obj)
         # TIDOC-1071 Modifications to support Cloud DocTree
         if opts.rest
           cls[:members].each do |member|
             if member[:tagname].eql? :method and not(member[:deprecated])
-              json.push({
+              member_obj = {
                 :name => cls[:name] + "." + member[:name],
                 :url => cls[:name] + "-method-" + member[:name],
-                :extends => cls[:extends],
-                :private => cls[:private] || cls[:meta][:pseudo],
-                :icon => "icon-component",
-                :isObject => "false"
-              })
+                :icon => "icon-component"
+              }
+              member_obj[:extends] = cls[:extends] if cls[:extends]
+              member_obj[:private] = true if cls[:private] || cls[:meta][:pseudo]
+              json.push(member_obj)
             end
           end
         end
